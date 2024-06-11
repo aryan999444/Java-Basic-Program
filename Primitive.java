2042c739c6441b131d8324c4fc37917ea31d6514
Aryan srivastava <108253903+aryan999444@users.noreply.github.com>
2024-06-11 11:42:23 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,10 @@
+public class Primitive {
+    public static void main(String[] args) {
+        int roll = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double LargeDecimalNumber = 4567654.456;
+        long LargeIntValues = 9451280460L;
+        boolean check = true;
+    }
+}
